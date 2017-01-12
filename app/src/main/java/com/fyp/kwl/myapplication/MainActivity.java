3a46813e1726583a430d2bed2a2b7e65da3b9a63
peter7764 <kwlkwl654@gmail.com>
2017-01-12 17:11:23 +0800

    finish main page

--- app/src/main/java/com/fyp/kwl/myapplication/MainActivity.java
+++ app/src/main/java/com/fyp/kwl/myapplication/MainActivity.java
@@ -2,6 +2,11 @@ package com.fyp.kwl.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listMainMenu();
+        clickOption();
+    }
+
+    private void listMainMenu() {
+        //  Create list of items
+        String[] showItems = {"Favourite", "Searching", "Setting"};
+
+        //  Build Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.menu_list_show_item, showItems);
+
+        //  Configure the list view to the Adapter
+        ListView lv = (ListView) findViewById(R.id.lvMainMenu);
+        lv.setAdapter(adapter);
+    }
+
+    private void clickOption() {
+        ListView lv = (ListView)findViewById(R.id.lvMainMenu);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        Toast.makeText(MainActivity.this, "favourite cliked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "search clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "setting clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 }
