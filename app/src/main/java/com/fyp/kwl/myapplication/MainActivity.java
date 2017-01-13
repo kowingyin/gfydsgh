998f95a57c338be657d153b41d7ed4b6b59d7c4b
peter7764 <kwlkwl654@gmail.com>
2017-01-13 20:02:49 +0800

    completed double click, waiting for new page

--- app/src/main/java/com/fyp/kwl/myapplication/MainActivity.java
+++ app/src/main/java/com/fyp/kwl/myapplication/MainActivity.java
@@ -14,13 +14,16 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    int timeOfClick = 0;
+    static int position;   //  position of clicking on listview
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        lv = (ListView)findViewById(R.id.lvMainMenu);
         listMainMenu();
         clickOption();
 
@@ -35,27 +38,69 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.menu_list_show_item, showItems);
 
         //  Configure the list view to the Adapter
-        ListView lv = (ListView) findViewById(R.id.lvMainMenu);
+
         lv.setAdapter(adapter);
     }
 
     private void clickOption() {
-        ListView lv = (ListView)findViewById(R.id.lvMainMenu);
+
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position){
-                    case 0:
-                        Toast.makeText(MainActivity.this, "favourite cliked", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 1:
-                        Toast.makeText(MainActivity.this, "search clicked", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 2:
-                        Toast.makeText(MainActivity.this, "setting clicked", Toast.LENGTH_SHORT).show();
-                        break;
-                }
+                timeOfClick++;
+                MainActivity.position = position;
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (timeOfClick == 1){
+                            switch (MainActivity.position){
+                                case 0:
+                                    Toast.makeText(MainActivity.this, "single favourite cliked", Toast.LENGTH_SHORT).show();
+                                    break;
+                                case 1:
+                                    Toast.makeText(MainActivity.this, "single search clicked", Toast.LENGTH_SHORT).show();
+                                    break;
+                                case 2:
+                                    Toast.makeText(MainActivity.this, "single setting clicked", Toast.LENGTH_SHORT).show();
+                                    break;
+                            }
+                        }else if (timeOfClick == 2){
+                            switch (MainActivity.position){
+                                case 0:
+                                    Toast.makeText(MainActivity.this, "favourite cliked", Toast.LENGTH_SHORT).show();
+                                    break;
+                                case 1:
+                                    Toast.makeText(MainActivity.this, "search clicked", Toast.LENGTH_SHORT).show();
+                                    break;
+                                case 2:
+                                    Toast.makeText(MainActivity.this, "setting clicked", Toast.LENGTH_SHORT).show();
+                                    break;
+                            }
+                        }
+                        timeOfClick = 0;
+                    }
+                } , 500);
             }
         });
+//        lv.setOnItemClickListener(new DoubleClickListener());
+//        new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                switch (position){
+//                    case 0:
+//                        Toast.makeText(MainActivity.this, "favourite cliked", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case 1:
+//                        Toast.makeText(MainActivity.this, "search clicked", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case 2:
+//                        Toast.makeText(MainActivity.this, "setting clicked", Toast.LENGTH_SHORT).show();
+//                        break;
+//                }
+//            }
+//        }
     }
 }
