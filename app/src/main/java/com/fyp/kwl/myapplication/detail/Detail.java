22cbdca6965b04c6aa47a6ff36324ea15ae4f078
peter7764 <kwlkwl654@gmail.com>
2017-01-15 19:54:22 +0800

    created detail

---
+++ app/src/main/java/com/fyp/kwl/myapplication/detail/Detail.java
@@ -0,0 +1,15 @@
+package com.fyp.kwl.myapplication.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fyp.kwl.myapplication.R;
+
+public class Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_detail);
+    }
+}
