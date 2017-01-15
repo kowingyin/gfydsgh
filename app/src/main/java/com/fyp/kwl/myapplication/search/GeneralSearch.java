8e43412f74ef39c15371b986cf4009cb1cb94d8c
peter7764 <kwlkwl654@gmail.com>
2017-01-15 16:56:25 +0800

    update general search

--- app/src/main/java/com/fyp/kwl/myapplication/search/GeneralSearch.java
+++ app/src/main/java/com/fyp/kwl/myapplication/search/GeneralSearch.java
@@ -1,13 +1,17 @@
 package com.fyp.kwl.myapplication.search;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.fyp.kwl.myapplication.MainActivity;
 import com.fyp.kwl.myapplication.R;
@@ -27,11 +31,12 @@ public class GeneralSearch extends AppCompatActivity {
         setContentView(R.layout.search_general_search);
 
         lv = (ListView)findViewById(R.id.lvGeneral);
-//        et = (EditText)findViewById(R.id.etSearchBuild);
+        et = (EditText)findViewById(R.id.etSearchBuild);
 
         ttsobject = MainActivity.ttsobject;
 
         listBuilding();
+        editBoxActions();
 
 
 //        lv.setTextFilterEnabled(true);
@@ -71,5 +76,52 @@ public class GeneralSearch extends AppCompatActivity {
         //  Configure the list view to the Adapter
 
         lv.setAdapter(adapter);
+    }
+    private void editBoxActions(){
+        et.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeOfClick++;
+                SearchMenu.position = position;
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //  output sound
+                        if (timeOfClick == 1){
+                            speak("搜尋列，雙擊使用語音輸入");
+                            //  open activity
+                        }else if (timeOfClick == 2){
+                            Intent intent = null;
+                            switch (GeneralSearch.position){
+                                case 0:
+//                                    Toast.makeText(MainActivity.this, "favorite cliked", Toast.LENGTH_SHORT).show();
+//                                    speak("move to general searching page");
+                                    speak("移至普通搜尋");
+                                    intent = new Intent(GeneralSearch.this, GeneralSearch.class);
+                                    break;
+                                case 1:
+//                                    Toast.makeText(MainActivity.this, "search clicked", Toast.LENGTH_SHORT).show();
+//                                    speak("move to selecting ");
+                                    speak("移至地區搜尋");
+                                    intent = new Intent(GeneralSearch.this, SearchMenu.class);
+                                    break;
+                            }
+                            startActivity(intent);
+                        }
+                        timeOfClick = 0;
+                    }
+                } , 500);
+
+            }
+        });
+    }
+    public void speak(String speakedText){
+        if ( result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA ){
+            Toast.makeText(this, "Feature not support on your device", Toast.LENGTH_SHORT).show();
+        }else {
+            ttsobject.speak(speakedText, TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 }
