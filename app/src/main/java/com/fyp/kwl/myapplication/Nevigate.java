d7fc613f5909d303d8bdbf56820e16f6ee5c3559
note <kwlkwl654@gmail.com>
2017-01-16 09:39:19 +0800

    final update

---
+++ app/src/main/java/com/fyp/kwl/myapplication/Nevigate.java
@@ -0,0 +1,15 @@
+package com.fyp.kwl.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nevigate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nevigate_nevigate);
+
+
+    }
+}
--- app/src/main/java/com/fyp/kwl/myapplication/detail/Detail.java
+++ app/src/main/java/com/fyp/kwl/myapplication/detail/Detail.java
@@ -1,17 +1,30 @@
 package com.fyp.kwl.myapplication.detail;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.fyp.kwl.myapplication.MainActivity;
+import com.fyp.kwl.myapplication.Nevigate;
 import com.fyp.kwl.myapplication.R;
+import com.fyp.kwl.myapplication.search.GeneralSearch;
 
 public class Detail extends AppCompatActivity {
     String building;
     TextView title, content;
     Button nevigation;
+    TextToSpeech ttsobject;
+    int timeOfClick = 0;
+    public int result;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
 
 
     @Override
@@ -20,6 +33,49 @@ public class Detail extends AppCompatActivity {
         setContentView(R.layout.detail_detail);
 
 //        building = Intent.getIntent("building");
+        nevigation = (Button) findViewById(R.id.button);
+        title = (TextView) findViewById(R.id.tvTitle);
+        content = (TextView) findViewById(R.id.tvContent);
+        ttsobject = MainActivity.ttsobject;
 
+        nevigate();
+    }
+
+    private void nevigate() {
+        nevigation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                final View v = view;
+                timeOfClick++;
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //  output sound
+                        if (timeOfClick == 1){
+                            speak("");
+                            //  open activity
+                        }else if (timeOfClick == 2){
+                            //  detail
+
+                        }
+                        timeOfClick = 0;
+                    }
+                } , 500);
+
+            }
+        });
+        Intent intent = new Intent(Detail.this, Nevigate.class);
+
+    }
+
+    public void speak(String speakedText){
+        if ( result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA ){
+            Toast.makeText(this, "Feature not support on your device", Toast.LENGTH_SHORT).show();
+        }else {
+            ttsobject.speak(speakedText, TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 }
