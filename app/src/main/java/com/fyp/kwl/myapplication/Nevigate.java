35734ce651d49a9c29fa3b7384d9840a7ee09333
note <kwlkwl654@gmail.com>
2017-01-16 09:58:04 +0800

    navigate update

--- app/src/main/java/com/fyp/kwl/myapplication/Nevigate.java
+++ app/src/main/java/com/fyp/kwl/myapplication/Nevigate.java
@@ -1,15 +1,17 @@
 package com.fyp.kwl.myapplication;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Nevigate extends AppCompatActivity {
+    TextToSpeech ttsobject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nevigate_nevigate);
 
-
+        ttsobject = MainActivity.ttsobject;
     }
 }
